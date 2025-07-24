2a0d2f469a2d52522446d266e32ecfbc69743dce
Anna Popova <anna.popova.de@gmail.com>
2025-07-24 15:59:55 +0200

    Add participant management functionality and currency constants

--- app/trip/create.tsx
+++ app/trip/create.tsx
@@ -2,6 +2,7 @@ import CustomButton from '@/components/CustomButton';
 import CustomInput from '@/components/CustomInput';
 import DatePicker from '@/components/DatePicker';
 import Dropdown from '@/components/Dropdown';
+import { createParticipant } from '@/lib/participants';
 import { createTrip } from '@/lib/trips';
 import useAuthStore from '@/store/auth.store';
 import { Currency, TripFormData } from '@/type';
@@ -10,14 +11,13 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
 import { Alert, Pressable, ScrollView, Text, View } from 'react-native';
-
-
+import { currencies } from '@/variables'; 
 
 const CreateTrip = () => {
-    
+
     const [isSubmitting, setIsSubmitting] = useState(false);
     const router = useRouter();
-    const {user} = useAuthStore(); // Assuming you have a user store to get the current user
+    const { user } = useAuthStore(); // Assuming you have a user store to get the current user
     
     const [formData, setFormData] = useState<TripFormData>({
         name: '',
@@ -27,16 +27,10 @@ const CreateTrip = () => {
     });
 
     const [participants, setParticipants] = useState<string[]>([user!.name]);
-    const [isAddParticipant, setIsAddParticipant] = useState(false);
+    const [addParticipantMode, setAddParticipantMode] = useState(false);
     const [participantName, setParticipantName] = useState('');
 
-    const currencies = [
-        { label: 'Euro (EUR)', value: 'EUR' },
-        { label: 'US Dollar (USD)', value: 'USD' },
-        { label: 'Ukrainian Hryvnia (UAH)', value: 'UAH' },
-        { label: 'Polish Złoty (PLN)', value: 'PLN' },
-    ];   
-    
+
     // Generic updater function
     const updateField = <K extends keyof TripFormData>(field: K, value: TripFormData[K]) => {
         setFormData(prev => ({
@@ -44,11 +38,16 @@ const CreateTrip = () => {
             [field]: value
         }));
     };
+    // ==============================================================
+    // PARTICIPANT MANAGEMENT
+    // ==============================================================
 
+    // Function to check for duplicate participants
     const isDuplicateParticipant = (name: string) => {
         return participants.some(participant => participant.toLowerCase() === name.toLowerCase());
     };
 
+    // Function to add a participant
     const addParticipantHandler = (name: string) => {
         if (!name.trim()) {
             return Alert.alert("Please enter participant name");
@@ -57,16 +56,43 @@ const CreateTrip = () => {
         if (isDuplicateParticipant(name)) {
             return Alert.alert("Participant already exists");
         }
+        // Add participant to the list 
+        if (participants.length >= 5) {
+            return Alert.alert("Maximum of 5 participants allowed");
+        }
         setParticipants(prev => [...prev, name]);
-        setIsAddParticipant(false);
+        // Reset the form
+        setAddParticipantMode(false);
         setParticipantName('');
     }
+    // Function to remove a participant from the list
+    const removeParticipantHandler = (index: number) => {
+        setParticipants(prev => prev.filter((_, i) => i !== index));
+    };
 
-      const submit = async () => {
-        const { name, startDate, endDate, defaultCurrency } = formData;
+    // Function to create a participant
+    const createParticipantHandler = async (name:string, tripId:string) => {
         
+            try {
+                await createParticipant({ name, tripId });
+            } catch (error) {
+                console.error("Error creating participant:", error);
+                Alert.alert("Error", "Failed to create participant");
+            }
+        };
+    // =============================================================
+    // TRIP CREATION
+    // =============================================================
+
+    const submit = async () => {
+        const { name, startDate, endDate, defaultCurrency } = formData;
+
         if (!name.trim()) {
             return Alert.alert("Error", "Please enter trip name");
+        }
+
+        if (participants.length === 0) {
+            return Alert.alert("Please add at least one participant");
         }
 
         setIsSubmitting(true);
@@ -78,10 +104,17 @@ const CreateTrip = () => {
                 defaultCurrency
             };
 
-            await createTrip(tripData);
+            const newTrip =await createTrip(tripData);
+            const tripId = newTrip.$id;
+
+            // Create participants for the trip
+            
+            for (const participant of participants) {
+                await createParticipantHandler(participant, tripId);
+            }
             Alert.alert("Trip created successfully!");
             router.replace("/");
-            console.log("Trip Created:", tripData);
+            //console.log("Trip Created:", tripData);
 
         } catch (error: any) {
             console.error("Creating Trip Error:", error);
@@ -95,24 +128,24 @@ const CreateTrip = () => {
 
     return (
         <ScrollView className='gap-10 bg-white rounded-lg p-5 mt-5'>
-            <CustomInput 
-                placeholder="Enter trip name" 
-                value={formData.name} 
-                label="Trip Name" 
+            <CustomInput
+                placeholder="Enter trip name"
+                value={formData.name}
+                label="Trip Name"
                 onChangeText={(text) => updateField('name', text)}
             />
-            
+
             <View>
                 <Text className="label">Start date</Text>
-                <DatePicker 
+                <DatePicker
                     value={formData.startDate}
                     onDateChange={(date) => updateField('startDate', date)}
                 />
             </View>
-            
+
             <View>
                 <Text className="label">End date</Text>
-                <DatePicker 
+                <DatePicker
                     value={formData.endDate}
                     onDateChange={(date) => updateField('endDate', date)}
                     minimumDate={formData.startDate}
@@ -134,7 +167,7 @@ const CreateTrip = () => {
                 {participants.length > 0 ? participants.map((participant, index) => (
                     <View key={index} className="flex-row items-center gap-5 my-2">
                         <Pressable
-                            onPress={() => setParticipants(prev => prev.filter((_, i) => i !== index))}
+                            onPress={() => removeParticipantHandler(index)}
                             className="px-2"
                         >
                             <FontAwesome name="remove" size={24} color="#e45f2b" />
@@ -142,45 +175,45 @@ const CreateTrip = () => {
                         <Text>{participant}</Text>
                     </View>
                 )) : (
-                    <Text className={!isAddParticipant ? "text-gray-500" : "hidden"}>No participants</Text>
+                    <Text className={!addParticipantMode ? "text-gray-500" : "hidden"}>No participants</Text>
                 )}
                 <View className='flex items-end'>
-                <Pressable
-                    onPress={() => setIsAddParticipant(true)}
-                    className={isAddParticipant ? 'hidden' : 'bg-secondary w-1/2 h-12 flex flex-row items-center justify-center rounded-xl my-2 gap-2'}
-                >
-                    <FontAwesome name="plus" size={16} color="white" />
-                    <Text className="text-white font-medium">Add Participant</Text>
-                </Pressable>
+                    <Pressable
+                        onPress={() => setAddParticipantMode(true)}
+                        className={addParticipantMode ? 'hidden' : 'bg-secondary w-1/2 h-12 flex flex-row items-center justify-center rounded-xl my-2 gap-2'}
+                    >
+                        <FontAwesome name="plus" size={16} color="white" />
+                        <Text className="text-white font-medium">Add Participant</Text>
+                    </Pressable>
                 </View>
 
-                { isAddParticipant && (
+                {addParticipantMode && (
                     <View>
-                    <CustomInput
-                        placeholder="Enter participant name"
-                        value={participantName}
-                        onChangeText={(text) => {setParticipantName(text)}}
-                    />
-                    <View className='flex-row justify-between'>
-                        <CustomButton
-                            text="Add"
-                            onPress={()=>addParticipantHandler(participantName)}
-                            classname='w-[45%] h-12 bg-secondary rounded-xl my-2 flex items-center justify-center'
+                        <CustomInput
+                            placeholder="Enter participant name"
+                            value={participantName}
+                            onChangeText={(text) => { setParticipantName(text) }}
                         />
-                        <CustomButton
-                            text="Cancel"
-                            onPress={()=>{setIsAddParticipant(false); setParticipantName('')}}
-                            classname='w-[45%] h-12 bg-tertiary rounded-xl my-2 flex items-center justify-center'
-                        />
+                        <View className='flex-row justify-between'>
+                            <CustomButton
+                                text="Add"
+                                onPress={() => addParticipantHandler(participantName)}
+                                classname='w-[45%] h-12 bg-secondary rounded-xl my-2 flex items-center justify-center'
+                            />
+                            <CustomButton
+                                text="Cancel"
+                                onPress={() => { setAddParticipantMode(false); setParticipantName('') }}
+                                classname='w-[45%] h-12 bg-tertiary rounded-xl my-2 flex items-center justify-center'
+                            />
                         </View>
                     </View>
                 )}
             </View>
-            
-            <CustomButton 
-                text="Create Trip" 
-                isLoading={isSubmitting} 
-                onPress={submit} 
+
+            <CustomButton
+                text="Create Trip"
+                isLoading={isSubmitting}
+                onPress={submit}
             />
         </ScrollView>
     );
---
+++ variables.js
@@ -0,0 +1,6 @@
+ export const currencies = [
+        { label: 'Euro (EUR)', value: 'EUR' },
+        { label: 'US Dollar (USD)', value: 'USD' },
+        { label: 'Ukrainian Hryvnia (UAH)', value: 'UAH' },
+        { label: 'Polish Złoty (PLN)', value: 'PLN' },
+    ];
